887c27b6e124492782be621a4d69ffbd14c2b8a9
Tom Erik <tomerik@Toms-MacBook-Pro.local>
2020-10-07 09:04:52 +0200

    Lagt til en annen navbar

--- resources/views/wishlist/create.blade.php
+++ resources/views/wishlist/create.blade.php
@@ -10,25 +10,17 @@
 <body>
 
     <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          
-            <div class="collapse navbar-collapse" id="navbarColor01">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('index') }}">Hjem</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="{{ route('shoppinglist.index') }}">Handleliste</a>
-                </li>
-                <li class="nav-item active">
-                  <a class="nav-link" href="{{ route('wishlist.index') }}">Ønskeliste</a>
-                  </li>
-              </ul>
-            </div>
-          </nav>
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('index') }}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('shoppinglist.index') }}">Handleliste</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="{{ route('wishlist.index') }}">Ønskeliste</a>
+        </li>
+      </ul>
         </div>
 
 <br>
